ae16f2b7940f7bbbc9a8e477e214dd69f213d1c7
Alex Quang <alexquang1991@gmail.com>
2016-07-06 16:57:12 -0700

    centered mobile menu button

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,23 @@
 import Ember from 'ember';
+const $ = Ember.$;
 
 export default Ember.Route.extend({
+	menuActive: false,
 	actions: {
 		scrollToElement: function(element) {
 			$('html, body').animate({
 				scrollTop: $('#' + element).offset().top - 50
 			}, 600);
+		},
+		openMobileMenu: function() {
+			if (this.get('menuActive') === false) {
+				this.set('menuActive', true);
+				$('.open-menu-btn').addClass('active');
+			}
+			else {
+				this.set('menuActive', false);
+				$('.open-menu-btn').removeClass('active');
+			}
 		}
 	}
 });
