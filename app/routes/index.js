1f4971d97e62ef0d34639eb1165cf86ee91a11dc
Alex Quang <alexquang1991@gmail.com>
2016-07-05 21:22:40 -0700

    sticky nav is working properly

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+		scrollToElement: function(element) {
+			$('html, body').animate({
+				scrollTop: $('#' + element).offset().top - 50
+			}, 600);
+		}
+	}
+});
+
+$(window).on('scroll', function() {
+	if ($(window).scrollTop() > 130) {
+		$('.main-links').addClass('fixed-nav');
+		$('.brand-wrapper').addClass('extended');
+	}
+	else {
+		$('.main-links').removeClass('fixed-nav');
+		$('.brand-wrapper').removeClass('extended');
+	}
 });
